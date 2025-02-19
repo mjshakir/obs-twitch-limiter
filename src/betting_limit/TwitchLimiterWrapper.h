7ecdbddbf9e176bea3ce9f688426b455c1f9af9f
mjshakir <maslove23@gmail.com>
2025-02-18 18:35:50 -0800

    Initial Commit

---
+++ src/betting_limit/TwitchLimiterWrapper.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <obs-module.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool TwitchLimiter_load(void);
+void TwitchLimiter_unload(void);
+obs_properties_t *TwitchLimiter_get_settings(void *data);
+void TwitchLimiter_update_settings(obs_data_t *settings);
+
+#ifdef __cplusplus
+}
+#endif
