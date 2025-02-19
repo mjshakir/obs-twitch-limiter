3afb0d274201604852900a59fc38e3005cd6c170
mjshakir <maslove23@gmail.com>
2025-02-18 18:14:41 -0800

    Change: remove the limiter header

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -18,7 +18,6 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 
 #include <obs-module.h>
 #include <plugin-support.h>
-#include "TwitchLimiter.hpp" // Include your C++ implementation
 
 #ifdef __cplusplus
 extern "C" {
