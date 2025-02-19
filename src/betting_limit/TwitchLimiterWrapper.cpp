27a5fb878048a171044bc22947dc73f2490ebb21
mjshakir <maslove23@gmail.com>
2025-02-18 20:21:10 -0800

    Fix: format

--- src/betting_limit/TwitchLimiterWrapper.cpp
+++ src/betting_limit/TwitchLimiterWrapper.cpp
@@ -5,22 +5,22 @@ extern "C" {
 
 bool TwitchLimiter_load(void)
 {
-    return TwitchLimiter::instance().initialized();
+	return TwitchLimiter::instance().initialized();
 }
 
 void TwitchLimiter_unload(void)
 {
-    TwitchLimiter::instance().shutdown();
+	TwitchLimiter::instance().shutdown();
 }
 
 obs_properties_t *TwitchLimiter_get_settings(void *data)
 {
-    return TwitchLimiter::instance().get_settings(data);
+	return TwitchLimiter::instance().get_settings(data);
 }
 
 void TwitchLimiter_update_settings(obs_data_t *settings)
 {
-    TwitchLimiter::instance().update_settings(settings);
+	TwitchLimiter::instance().update_settings(settings);
 }
 
 } // extern "C"
