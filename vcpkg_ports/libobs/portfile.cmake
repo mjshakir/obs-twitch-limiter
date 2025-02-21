1b94cf9c1ab2d0e20458acda55a2bad0f8b16fa8
mjshakir <maslove23@gmail.com>
2025-02-21 00:43:58 -0800

    Fix: pathing

--- vcpkg_ports/libobs/portfile.cmake
+++ vcpkg_ports/libobs/portfile.cmake
@@ -2,8 +2,9 @@
 if(NOT DEFINED ENV{LIBOBS_INSTALL_DIR})
     message(FATAL_ERROR "Please set the LIBOBS_INSTALL_DIR environment variable to the libobs install directory.")
 endif()
-# Convert LIBOBS_INSTALL_DIR to a CMake path:
 file(TO_CMAKE_PATH "$ENV{LIBOBS_INSTALL_DIR}" LIBOBS_ROOT)
+file(INSTALL ${LIBOBS_ROOT} DESTINATION ${CURRENT_PACKAGES_DIR}/lib/cmake/libobs)
+
 
 # Install the configuration files (and headers, if needed)
 # Adjust the installation commands if your libobs install has a different layout.
