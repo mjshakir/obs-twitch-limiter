068185774bc86f8f06cfa7617bf407a3c40d326e
mjshakir <maslove23@gmail.com>
2025-02-21 13:24:46 -0800

    Fix: the file name

--- .github/scripts/fetch-deps.ps1
+++ .github/scripts/fetch-deps.ps1
@@ -23,7 +23,7 @@ $dep = $buildspec.dependencies.prebuilt
 
 # Construct the file name and URL for Windows x64.
 # This example assumes the file is named "<version>-windows-x64.zip"
-$fileName = "$($dep.version)-windows-x64.zip"
+$fileName = "obs-deps-$($dep.version)-windows-x64.zip"
 $url = "$($dep.baseUrl)/$($dep.version)/$fileName"
 
 Write-Host "Downloading prebuilt obs-deps from $url ..."
